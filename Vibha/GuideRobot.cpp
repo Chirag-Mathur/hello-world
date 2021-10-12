cb2cde411b0a3206480109e11709a2c38a48b04e
vibha-v-657 <60540294+vibha-v-657@users.noreply.github.com>
2021-10-12 12:20:01 +0530

    Create GuideRobot.cpp

---
+++ Vibha/GuideRobot.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//guide a robot to the destination for (0,0) using only right or downward moves
+int r,c;
+//recursion
+int ways(int i,int j)
+{
+    if(i==r-1 && j==c-1)
+        return 1;
+    if(i>=r || j>=c)
+        return 0;
+    return ways(i+1,j)+ways(i,j+1);
+}
+
+int main()
+{
+    cout<<"Enter the number of rows in the square grid:"<<endl;
+    cin>>r;
+    c=r;
+    int m=0;
+    m=ways(0,0);
+    cout<<m<<endl;
+    
+    return 0;
+}
+
